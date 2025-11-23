a52e13a5d35f284238c25541edacb7e367f93e9f
ShantiMishra <shantimishra2006@gmail.com>
2025-11-23 11:22:36 +0530

    Binary Search

--- array/binary_search.cpp
+++ array/binary_search.cpp
@@ -4,14 +4,39 @@ using namespace std;
 int binarySearch(int arr[], int size , int key){
     int start=0;
     int end=size-1;
-    int mid =(start + end)
+    int mid =(start + end)/2;
+    while(start<=end){
+        if(arr[mid]==key){
+            return mid;
+        }
+        if(arr[mid]<key){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+        mid=(start+end)/2;
+    }
+    return -1;
 }
 
 
 int main(){
 
-    int even[6]={2,4,6,8,12,10};
+    int even[6]={2,4,6,8,10,12};
     int odd[5]={3,8,11,14,16};
+    int n= sizeof(even)/sizeof(even[0]);
+    int m= sizeof(odd)/sizeof(odd[0]);
+    int key=8;
+    // int result= binarySearch(even,n,key);
+    int result= binarySearch(odd,m,key);
+
+    if (result==-1){
+        cout<<"Element not found";
+    }
+    else{
+        cout<<"Element found at : "<<result;
+    }
 
 
 }
