36ef5e8bb476f2cbf794c942aa31e7faf34180e4
ShantiMishra <shantimishra2006@gmail.com>
2025-12-04 23:17:43 +0530

    Bubble Sort

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int bubbleSort(int arr[], int n){
     for(int i=1; i<n;i++){
+        //for round 1 to n-1
+
         for(int j=0; j<n-i; j++){
+            //process the element till (n-i)th index
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
