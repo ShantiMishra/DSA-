ac3632d077be056c18981fd5dc8f5221423b68b6
ShantiMishra <shantimishra2006@gmail.com>
2025-12-04 15:48:19 +0530

    Selection Sort

--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int selectionSort(int arr[], int n){
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n-1;i++){
         int minIndex = i;
         for(int j=i+1; j<n;j++){
             if(arr[j]<arr[minIndex]){
@@ -21,7 +21,7 @@ int printArray(int arr[], int n){
 
 
 int main(){
-     int arr[5]={3,1,9,6,2};
+     int arr[5]={3,10,9,6,2};
      int n = sizeof(arr)/sizeof(arr[0]);
      selectionSort(arr,n);
      printArray(arr,n);
