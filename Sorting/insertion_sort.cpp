72280d1c9d1427e2b51dde0595faf56bd1101df8
ShantiMishra <shantimishra2006@gmail.com>
2025-12-06 08:53:38 +0530

    Insertion Sort

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int insertionSort(int arr[] , int n){
+    for(int i=1; i<n; i++){
+        int temp = arr[i];
+         int j=i-1;
+         while(j>=0 && arr[j]>temp){
+            arr[j+1]=arr[j];
+            j--;
+         }
+         arr[j+1]=temp;
+    }
+}
 
-
+int printArray(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
 
 
 int main(){
+      int arr[7] = {10,1,7,4,8,2,11};
+      int n = sizeof(arr)/sizeof(arr[0]);
+      insertionSort(arr,n);
+      printArray(arr,n);
 
 }
