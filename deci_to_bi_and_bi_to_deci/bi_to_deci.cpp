b17622fba2e4459d92ba63edbc035c1c266c3d44
ShantiMishra <shantimishra2006@gmail.com>
2025-12-18 22:03:47 +0530

    Conflict Resolved

--- deci_to_bi_and_bi_to_deci/bi_to_deci.cpp
+++ deci_to_bi_and_bi_to_deci/bi_to_deci.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <math.h>
 using namespace std;
@@ -18,25 +17,4 @@ int main(){
 
     }
     cout<< ans << endl;
-=======
-#include <iostream>
-#include <math.h>
-using namespace std;
-int main(){
-    int n;
-    cout<<"Enter the valu of n :";
-    cin>>n;
-    int i=0, ans=0;
-
-    while(n!=0){
-      int digit = n%10;
-      if(digit==1){
-           ans= ans + pow(2,i);
-      }
-      n = n/10;
-      i++;
-
-    }
-    cout<< ans << endl;
->>>>>>> 8da0815a360d05d4b4653cfa45ff895e89448c0a
 }
