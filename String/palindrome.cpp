ecf315ea08bed9fee26dbe017f89d3de19e94da9
ShantiMishra <shantimishra2006@gmail.com>
2025-12-18 22:18:34 +0530

    Palindrome or Not

---
+++ String/palindrome.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+bool checkPalindrome(char a[] , int n){
+    int s = 0;
+    int e = n-1;
+    while(s<=e){
+        if(a[s]==a[e]){
+            s++;
+            e--;
+        }
+        else{
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(){
+    char name[20];
+    cout<<"Enter Your name : ";
+    cin>>name;
+    int n = strlen(name);
+    bool data = checkPalindrome(name,n);
+    if(data==1){
+        cout<<"Palindrome";
+    }
+    else{
+        cout<<"Not a Palindrome";
+    }
+}
