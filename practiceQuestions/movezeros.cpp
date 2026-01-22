1abc7fc5ea350cb75a97e7560dc8fcaadb225631
ShantiMishra <shantimishra2006@gmail.com>
2026-01-22 22:31:48 +0530

    move zeros to end

---
+++ practiceQuestions/movezeros.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std ;
+
+void moveZeros(int arr[], int n){
+      int j=0;
+      for (int i=0 ; i<n ;i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+      }
+     
+}
+
+
+
+int main(){
+    int arr[9]={10,20,0,30,40,0,0,50,60};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    moveZeros(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+}
