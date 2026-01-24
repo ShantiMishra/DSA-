103967b2e7b734dbef60c223e2794ee61f1acdac
ShantiMishra <shantimishra2006@gmail.com>
2026-01-24 09:29:20 +0530

    Contains Duplicate or not

---
+++ practiceQuestions/ContainsDuplicate.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+bool containDuplicate(int arr[], int n){
+    sort(arr,arr+n);
+    for(int i=0; i<n;i++){
+        if(arr[i]==arr[i+1]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+
+
+int main (){
+   int n;
+   cout<<"Enter the value of n : ";
+   cin>>n;
+   int arr[n];
+   cout<<"enter the elements of array : "<<" ";
+   for(int i=0;i<n;i++){
+    cin>>arr[i];
+   }
+
+   bool result =containDuplicate(arr,n);
+   if(result==true){
+    cout<<"Contains";
+   }
+   else{
+    cout<<"Not Contains";
+   }
+}
