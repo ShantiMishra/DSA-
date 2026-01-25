25207c50d58fb0608283815fe1b24fbb16cdb2a5
ShantiMishra <shantimishra2006@gmail.com>
2026-01-25 11:42:58 +0530

    Updated i

--- practiceQuestions/ContainsDuplicate.cpp
+++ practiceQuestions/ContainsDuplicate.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 bool containDuplicate(int arr[], int n){
     sort(arr,arr+n);
-    for(int i=0; i<n;i++){
-        if(arr[i]==arr[i+1]){
+    for(int i=1; i<n;i++){
+        if(arr[i]==arr[i-1]){
             return true;
         }
         else{
