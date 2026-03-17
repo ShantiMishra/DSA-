d30d95045cd707fd6d05cfd538882a74175889d0
ShantiMishra <shantimishra2006@gmail.com>
2026-03-17 20:52:57 +0530

    matrix diagonal sum

---
+++ 2D_Array/matrixDiagonalSum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std ;
+
+ int diagonalSum(vector<vector<int>>& mat) {
+        int n= mat.size();
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=mat[i][i];
+            sum+=mat[i][n-1-i];
+
+            if(i==n-1-i){
+                sum-=mat[i][n-1-i];  // i=n-1-i
+            }
+
+        }
+        return sum;
+
+    }
+
+
+
+int main(){
+
+}
