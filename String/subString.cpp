eceafb260ec39fc1b578e8dbb4701709eb730ae3
ShantiMishra <shantimishra2006@gmail.com>
2026-04-10 13:59:23 +0530

    subString

--- String/subString.cpp
+++ String/subString.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std ;
 int main(){
-    
+
     string s= "hello";
     int n= s.length();
    for(int i=0;i<n;i++){
      for (int j=1 ; j<=n-i;j++){
-        cout<< s.substr(0,j);
+        cout<< s.substr(i,j) << endl;
      }
    }
    return 0;
